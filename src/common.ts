ff7353f6e361c73bbb3ba0509546ffa18528832e
Alexis Cheron <git@norech.com>
2022-01-24 18:00:00 +0400

    added more module prefixes

--- src/common.ts
+++ src/common.ts
@@ -1,7 +1,7 @@
 export type LowercaseAlphabetLetter = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z";
 export type UppercaseAlphabetLetter = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z";
 
-export type ModulePrefix = "B" | "M" | "C" | "G";
+export type ModulePrefix = "B" | "M" | "C" | "G" | "W" | "T";
 
 export type YearLiteral = `${number}`;
 export type MonthLiteral = `${number}`;
